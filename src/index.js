2f910f664b91ca7edcdebfc7a025d7bbc0ff8e7a
BarbaraISC <barbaraisc93@gmail.com>
2025-02-13 10:15:27 +0000

    Added the AI api integration

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
-function generateRecipe(event) {
-  event.preventDefault();
-
-  new Typewriter("#recipe", {
-    strings: "The recipe will go here",
+function displayQuote(response) {
+  new Typewriter("#quote", {
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
 }
 
-let recipeFormElement = document.querySelector("#recipe-generator-form");
-recipeFormElement.addEventListener("submit", generateRecipe);
+function generateQuote(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "43b563a3f016fb0f8t1650905fb6o494";
+
+  let context =
+    "You are a book expert and love to write the most inspiring book quotes";
+  let prompt = `User instructions: Generate a book quote with ${instructionsInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayQuote);
+}
+
+let quoteFormElement = document.querySelector("#quote-generator-form");
+quoteFormElement.addEventListener("submit", generateQuote);
